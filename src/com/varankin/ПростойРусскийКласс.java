5620be68b2d0edb1fbdc57bdc96f2a4c87b46766
Nikolai Varankine <nvaranki@tut.by>
2021-10-23 20:32:28 +0300

    added project files

---
+++ src/com/varankin/ПростойРусскийКласс.java
@@ -0,0 +1,15 @@
+package com.varankin;
+
+/**
+ *
+ * @author © 2021 Николай Варанкин
+ */
+public class ПростойРусскийКласс 
+{
+    int поле;
+    
+    int метод( float параметр )
+    {
+        return поле;
+    }
+}
